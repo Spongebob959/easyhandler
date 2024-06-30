40f01f1389390f2e19e525233c2bc336f6695536
Spongebob959 <146587218+Oogway959@users.noreply.github.com>
2024-06-30 14:15:17 +0200

    Update easyhandler.go

--- easyhandler.go
+++ easyhandler.go
@@ -1,9 +1,11 @@
-package easyhandler
+package main
 
 import (
 	"fmt"
 	"reflect"
 )
+
+
 
 type Result[T any] struct {
 	Values []T
@@ -25,7 +27,7 @@ func (r *Result[T]) IsErr() (bool) {
 	return r.Err != nil
 }
 
-func handleArgs(args ...interface{}) ([]reflect.Value) {
+func HandleArgs(args ...interface{}) ([]reflect.Value) {
 	inputs := make([]reflect.Value, len(args))
 	for i, arg := range args {
 		inputs[i] = reflect.ValueOf(arg)
@@ -33,7 +35,7 @@ func handleArgs(args ...interface{}) ([]reflect.Value) {
 	return inputs
 }
 
-func Wrap(function interface{}, args ...interface{}) func() Result[any] {
+func Wrap(function interface{}, args ...interface{}) (func() Result[any]) {
 	return func() Result[any] {
 		var funcValue = reflect.ValueOf(function)
 		var funcType = funcValue.Type()
@@ -43,7 +45,7 @@ func Wrap(function interface{}, args ...interface{}) func() Result[any] {
 		if len(args) != funcType.NumIn() {
 			return Err[any](fmt.Errorf("argument count does not match function's parameter count"))
 		}
-		inputs := handleArgs(args...)
+		inputs := HandleArgs(args...)
 		var results = funcValue.Call(inputs)
 		if funcType.NumOut() == 0 {
 			return Ok[any]()
@@ -64,15 +66,34 @@ func Wrap(function interface{}, args ...interface{}) func() Result[any] {
 	}
 }
 
-func Try(handler func(error), fns ...func() Result[any]) ([]any, Result[any]) {
+type FuncValues struct {
+	Args []reflect.Value
+	Func *reflect.Value
+}
+
+func wrapErrHandler(handlerfunc interface{}, args ...interface{}) (Result[FuncValues]) {
+	var handlerValue = reflect.ValueOf(handlerfunc) 
+	var handlerType = handlerValue.Type()
+	var in = HandleArgs(args...)
+	if handlerType.Kind() != reflect.Func {
+		return Err[FuncValues](fmt.Errorf("provided handler is not a function"))
+	}
+	if handlerType.NumIn() != 1 || handlerType.In(0) != reflect.TypeOf((*error)(nil)).Elem() {
+		return Err[FuncValues](fmt.Errorf("the error handler must take an error as an arg"))
+	}
+	return Result[FuncValues]{ Values: []FuncValues{ { Args: in, Func: &handlerValue, }, } ,}
+}
+
+func Try(handler interface{}, fns ...func() Result[any]) ([]any, Result[any]) {
 	var results = []any{}
+	var handlerfunc = wrapErrHandler(handler)
 	if len(fns) == 0 {
 		return nil, Err[any](fmt.Errorf("no functions provided"))
 	}
 	for i := 0; i < len(fns); i++ {
 		var res = fns[i]()
 		if res.IsErr() {
-			handler(res.Err)
+			_ = handlerfunc.Values[0].Func.Call(handlerfunc.Values[0].Args)
 		} else {
 			results = append(results, res.Values...)
 		}
